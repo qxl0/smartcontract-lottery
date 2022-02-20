845e9e7db60155de657be85dd63ba4dfe6d1bebd
Qiang <qxl0@yahoo.com>
2022-02-20 14:43:00 -0500

    Added a test on start_enter lottery

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -23,3 +23,11 @@ def test_cant_enter_unless_started():
   with pytest.raises(exceptions.VirtualMachineError):
     lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
 
+def test_can_start_and_enter_lottery():
+  if (network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS):
+    pytest.skip()
+  lottery = deploy_lottery() 
+  account = get_account()
+  lottery.startLottery({"from": account})
+  lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+  assert lottery.players[0] == account
