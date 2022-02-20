ef38d7eb68b575083f1450d3f43f5bd8ec04474b
Qiang <qxl0@yahoo.com>
2022-02-19 21:08:03 -0500

    Added ownership and ownable

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,19 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
 
     constructor(address _priceFeedAddress) public {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
-    function enter() public {
+    function enter() public payable {
         // $50 minimum
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value > getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
 
@@ -27,7 +37,13 @@ contract Lottery {
         return costToEnter;
     }
 
-    function startLottery() public {}
+    function startLottery() public {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "CAn't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
     function endLottery() public {}
 }
