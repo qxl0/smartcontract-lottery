fc862b392a5a969d6dd95e6cb26837cdd61e18cb
Qiang <qxl0@yahoo.com>
2022-02-19 19:43:36 -0500

    Add Lottery.sol

---
+++ contracts/Lottery.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.6.6;
+
+contract Lottery {
+    address payable[] public players;
+
+    function enter() public {}
+
+    function getEntranceFee() public {}
+
+    function startLottery() public {}
+
+    function endLottery() public {}
+}
